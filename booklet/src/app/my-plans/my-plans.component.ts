494db9988ca4e8059ebad2c505ba66814353ca21
Adauto Barros <adauto.neto@ccc.ufcg.edu.br>
2019-10-04 08:42:55 -0300

    my plans page

--- booklet/src/app/my-plans/my-plans.component.ts
+++ booklet/src/app/my-plans/my-plans.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, CollectionReference } from '@angular/fire/firestore';
+import { Plan } from '../_models/plan.model';
+import { AuthService } from '../_services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-my-plans',
@@ -6,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-plans.component.scss'],
 })
 export class MyPlansComponent implements OnInit {
+  planCollectionRef: CollectionReference;
+  plan$: Observable<unknown[]>;
 
-  constructor() { }
+  constructor(
+    private afs: AngularFirestore,
+    private authService: AuthService
+  ) { }
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.plan$ = this.afs.collection('plans', 
+      ref => ref.where('teacher', '==', this.authService.userDetails().displayName)).valueChanges();
+  }
 }
